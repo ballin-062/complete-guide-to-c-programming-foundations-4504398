56c8961fe5b39bdafd1b4af7e5b8522f1be9ba78
ballin-062 <colin.l.torbett-1@ou.edu>
2025-01-15 00:18:38 +0000

    first commit

--- CH01/01_12/01_12-challenge1.c
+++ CH01/01_12/01_12-challenge1.c
@@ -1,9 +1,21 @@
-main program
- 
-initialization
+// main program
+#include <stdio.h>
+#include <stdlib.h>
 
-initial message
+int main()
+{
+  // initialization
+  // initial message
+  char string[] = "this is a message.";
+  int b = 4;
+  // main program loop
+  for (int a = 0; a < b; a++)
+  {
+    printf("%d\n", a);
+  }
 
-main program loop
+  // output results
+  printf("The results.\n");
 
-output results
+  return 0;
+}
